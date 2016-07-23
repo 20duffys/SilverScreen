af40e39f95854f106a8f3ea99b1b59a61879908c
Sam Duffy <[Asduffy20@stanford.edu>
2016-07-23 13:24:34 -0400

    work work work work

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+
+
+var express = require('express');
+var cors = require('cors');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var mongodb = require('mongodb');
+var app = express();
---
+++ frontend/index.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+  console.log('Silver screen!');
+
+  // ***************** part 1: Silverscreen API ***************** //
+
+  // Get the elements:
+  var searchBox = document.getElementById('search-box');
+  var searchBoxDiv = document.getElementById('search-box-div');
+  var searchBtn = document.getElementById('search-btn');
+  var charsInfoContainer = document.getElementById('info-container-characters');
+  var addToFavesBtn = document.getElementById('add-to-faves-btn');
+  var nameAddToFaves = '';
